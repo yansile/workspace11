5046638680f0464db6dfe24ce6abae673e2cf607
yansile <19918816878@163.com>
2021-11-15 16:20:48 +0800

    Revert "第一次"
    This reverts commit 75e2468b

--- src/Dome/Dome25.java
+++
@@ -1,5 +0,0 @@
-package Dome;
-//、编程输出九九乘法表。
-public class Dome25 {
-
-}
--- src/Extends/DomesticAddress.java
+++
@@ -1,9 +0,0 @@
-package Extends;
-//国内地址的格式为：邮政编码、国家、 省、城市、街道名，
-public class DomesticAddress extends Address {
-    public DomesticAddress(){}
-    public String display(){
-        return num+","+country+s+cs+jd;
-    }
-
-}
